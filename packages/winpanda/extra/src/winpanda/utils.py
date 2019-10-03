901e8b905cb7e548b1c68554247dd9411946ff1e
Oleksiy <Oleksiy_pashkovskiy@epam.com>
2019-10-03 12:30:31 +0300

    pritifying utils

--- packages/winpanda/extra/src/winpanda/utils.py
+++ packages/winpanda/extra/src/winpanda/utils.py
@@ -1,9 +1,6 @@
-
-""" 
-Download and unpack use example
+"""Download and unpack use example
 
 from utils import *
-
 url = "https://wintesting.s3.amazonaws.com/testing/packages/adminrouter/adminrouter--19c887e02f1a8325cabd980f039015b70ab19cee.tar.xz"
 path = "./tmp/"
 if is_downloadable(url):
@@ -15,17 +12,16 @@ print("this is {}".format(archive))
 unpack(archive, "./tmp/Arc/")
 
 """
+import os
+from pprint import pprint as pp
+import tarfile
 
 import requests
 from pySmartDL import SmartDL
-import os
-import tarfile
-from pprint import pprint as pp
+
 
 def is_downloadable(url):
-    """
-    Does the url contain a downloadable resource
-    """
+    """Does the url contain a downloadable resource."""
     h = requests.head(url, allow_redirects=True)
     header = h.headers
     content_type = header.get('content-type')
@@ -36,22 +32,18 @@ def is_downloadable(url):
     return True
 
 def download(url, location):
-    """
-    Downloads from url to location
-    uses  pySmartDL from https://pypi.org/project/pySmartDL/
+    """Downloads from url to location
+    uses  pySmartDL from https://pypi.org/project/pySmartDL/.
     """
     _location = os.path.abspath(location)
     dl = SmartDL(url, _location)
     dl.start()
     path = os.path.abspath(dl.get_dest())
-    print ("Downloaded to {} ".format(path), " from {}".format(url),sep='\n')
+    print("Downloaded to {} ".format(path), " from {}".format(url), sep='\n')
     return path
 
 def unpack(tarpath, location):
-    """
-    unpacks tar.xz to  location
-    """
-    
+    """Unpacks tar.xz to  location."""
     _location = os.path.abspath(location)
 
     if  not os.path.exists(_location):
@@ -60,7 +52,6 @@ def unpack(tarpath, location):
 
     with tarfile.open(tarpath) as tar:
         tar.extractall(_location)
-        print("extracted to {}".format(_location)) 
+        print("extracted to {}".format(_location))
         pp({tarinfo.name:tarinfo.size for tarinfo in tar})
     return _location
-
